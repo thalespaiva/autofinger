3318d6a64d204c448d98177e2dfdf86237d6e9b8
Thales Paiva <thalespaiva@gmail.com>
2015-01-24 20:10:27 -0200

    Tuning scientific notation to C.

--- note.py
+++ note.py
@@ -9,11 +9,12 @@ class Note(object):
         3 <-> B1
     """
 
-    base_black_keys = [2, 5, 7, 10, 12]
-    base_notes = dict([('a', 1), ('a#', 2), ('b', 3), ('c', 4), ('c#', 5),
-                       ('d', 6), ('d#', 7), ('e', 8), ('f', 9), ('f#', 10),
-                       ('g', 11), ('g#', 12)])
+    base_notes = dict([('c', 1), ('c#', 2), ('d', 3), ('d#', 4),
+                       ('e', 5), ('f', 6), ('f#', 7), ('g', 8),
+                       ('g#', 9), ('a', 10), ('a#', 11), ('b', 12)])
+
     number_of_base_notes = len(base_notes)
+    base_black_keys = [2, 4, 7, 9, 11]
 
     def __init__(self, note, hold=False):
         super(Note, self).__init__()
--- test_note.py
+++ test_note.py
@@ -7,12 +7,12 @@ import note
 
 class NoteTest(unittest.TestCase):
 
-    def test_pitch_of_f_sharp_4_is_46(self):
-        self.assertEqual(note.Note.get_pitch('F#4'), 46)
+    def test_pitch_of_f_sharp_4_is_43(self):
+        self.assertEqual(note.Note.get_pitch('F#4'), 43)
 
     def test_a_sharp_is_a_black_key(self):
         a_sharp_2 = note.Note('A#2')
-
+        print(note.Note.base_black_keys)
         self.assertTrue(a_sharp_2.is_a_black_key())
 
     def test_c_is_not_a_black_key(self):
@@ -20,18 +20,18 @@ class NoteTest(unittest.TestCase):
 
         self.assertFalse(c_4.is_a_black_key())
 
-    def test_f_sharp_3_has_the_same_pitch_as_F_sharp_3_equals_34(self):
+    def test_f_sharp_3_has_the_same_pitch_as_F_sharp_3_equals_31(self):
         f_sharp_3 = note.Note('f#3')
         F_sharp_3 = note.Note('F#3')
 
         self.assertEqual(F_sharp_3.pitch, f_sharp_3.pitch)
-        self.assertEqual(f_sharp_3.pitch, 34)
+        self.assertEqual(f_sharp_3.pitch, 31)
 
     def test_distance_between_b1_and_g2_is_20(self):
         b_1 = note.Note('b1')
         g_2 = note.Note('g2')
 
-        self.assertEqual(note.Note.distance(b_1, g_2), 20)
+        self.assertEqual(note.Note.distance(b_1, g_2), 8)
 
     def test_random_note_is_valid(self):
         r = note.Note.random_note()
