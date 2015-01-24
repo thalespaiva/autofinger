9359b4063b88848defd813e0d0a683ac4933afef
Thales Paiva <thalespaiva@gmail.com>
2015-01-24 19:04:38 -0200

    First unit test for unit module.

---
+++ test_unit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import unittest
+from fractions import Fraction
+
+import unit
+from note import Note
+
+
+class UnitTest(unittest.TestCase):
+
+    def test_a3_b4_c2_is_sorted_to_c2_a3_b4(self):
+        a_3 = Note('a3')
+        b_4 = Note('b4')
+        c_2 = Note('c2')
+
+        u = unit.Unit([a_3, b_4, c_2], Fraction(1, 2))
+        print(u)
+        self.assertEqual(u.notes, [c_2, a_3, b_4])
+
+if __name__ == "__main__":
+    unittest.main()
