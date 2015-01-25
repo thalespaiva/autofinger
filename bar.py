eb9482c3e07cfe7173111114bf8ae17a4665db1b
Thales Paiva <thalespaiva@gmail.com>
2015-01-24 22:02:05 -0200

    ton -> key

--- bar.py
+++ bar.py
@@ -5,24 +5,24 @@ import unit
 
 class Bar(object):
     """A Bar is a list of Units"""
-    def __init__(self, tonality, time_signature, units):
+    def __init__(self, key, time_signature, units):
         super(Bar, self).__init__()
         self.units = units
-        self.tonality = tonality
+        self.key = key
         self.time_signature = time_signature
 
     def __str__(self):
         out = ''
-        out += ' - - - - ' + str(self.time_signature) 
-        out += ' - - - - ' + str(self.tonality) + ' - - - -\n'
+        out += ' - - - - ' + str(self.time_signature)
+        out += ' - - - - ' + str(self.key) + ' - - - -\n'
         for u in self.units:
             out += str(u) + '\n'
         return out
 
     def random_bar():
-        tonality = 'c major'
+        key = 'c major'
         time_signature = '4/4'
         n_units = 8
         units = [unit.Unit.random_unit() for _ in range(n_units)]
 
-        return Bar(tonality, time_signature, units)
+        return Bar(key, time_signature, units)
